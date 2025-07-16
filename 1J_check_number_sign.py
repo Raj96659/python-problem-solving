16433e999d4942a621aee8c0638fcaee1910cdc5
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-07-16 10:49:11 +0530

    Create 1J_check_number_sign.py

---
+++ 1J_check_number_sign.py
@@ -0,0 +1,10 @@
+# Write a Python Program to Check if a Number is Positive, Negative or Zero.
+
+num = int(input("Enter the number : "))
+
+if num>0:
+    print('Positive')
+elif num<0:
+    print('Negative')
+else:
+    print('Number is Zero')
