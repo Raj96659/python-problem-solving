fe97d463cffb987277934e056ad7d6311617abdd
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-07-20 22:09:15 +0530

    Create 3A_split_and_join_string.py

---
+++ 3A_split_and_join_string.py
@@ -0,0 +1,11 @@
+#  Write a Python program to split and join a string
+
+input_str = input("Enter the string : ")
+
+split_input_str = input_str.split()
+
+print(split_input_str)
+
+join_split_input_str = " ".join(split_input_str)
+
+print(join_split_input_str)
